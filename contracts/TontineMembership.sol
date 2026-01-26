3aeb868699a80a49c3da9b63dfd3b3f5b7b4af5d
MENSAH Olivier <yao-olivier-desire.mensah@supinfo.com>
2026-01-26 09:00:35 +0100

    ajout des modifiers et des fonctions de mappings

--- contracts/TontineMembership.sol
+++ contracts/TontineMembership.sol
@@ -9,9 +9,9 @@ import "@openzeppelin/contracts/Access/AccessControl.sol";
 
 contract TontineMembership is ERC721, ERC721URIStorage, AccessControl {
     //je vais definir un role pour l'administrateur de la tontine
-    bytes32 public constant ADMIN_ROLE = keccak256("PRESIDENT_ROLE");
+    bytes32 public constant PRESIDENT_ROLE = keccak256("PRESIDENT_ROLE");
     uint256 private _tokenIdCounter;
-    Struct Membership {
+    Struct Memberinfo {
         string name;
         uint256 montantCotisation; //c'est le value car c'est la valeur financiere de l'adhesion a la tontine
         address avaliseur;
@@ -27,15 +27,36 @@ contract TontineMembership is ERC721, ERC721URIStorage, AccessControl {
         uint8 partsCount;               // Nombre de parts (max 2)
         string  uri; // URI pointant vers les metadonnees du NFT
     }
-
-    constructor() ERC721("TontineMembership", "TONTINE") {
-        //l'administrateur du contrat est celui qui le deploye
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(ADMIN_ROLE, msg.sender);
+    //definir les mapping pour stocker les informations des membres
+    mapping(uint256 => Memberinfo) public members;
+    mapping(address => uint256[]) public memberTokens;
+    mapping(uint256 => uint256) public avalisationcount; //nombre de personnes avalisés pour un tokenId donné
+    mapping(uint256 => address[]) public avalisedMembers; //liste des adresses avalisées pour un tokenId donné
+     // Limite de parts par membre
+    uint8 public constant MAX_PARTS_PER_MEMBER = 2;
+    
+    // Limite d'avalisation par membre
+    uint8 public constant MAX_AVALISATION_PER_MEMBER = 3;
+    // ============ MODIFICATEURS ============
+    //verifier que le token existe bien sur la blockchain
+    modifier onlyvalidMember(uint256 tokenId) {
+        require(_exists(tokenId), "Token ID does not exist");
+        _;
+    }
+    //verifier que le membre est actif et quil n'est pas suspendu ou inactif
+    modifier onlyActiveMember(uint256 tokenId) {
+        require(members[tokenId].status == MemberStatus.ACTIVE, "Membre non actif");
+        _;
     }
 
+    constructor() ERC721("TontineMembership", "TONTINE") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PRESIDENT_ROLE, msg.sender);
+        _tokenIdCounter = 1; //commencer a 1 pour eviter le tokenId 0
+    }
+ 
     //fonction pour mint un nouveau NFT d'adhesion a la tontine
-    function mintMembership(address to, string memory uri) public onlyRole(ADMIN_ROLE) {
+    function mintMembership(address to, string memory uri) public onlyRole(PRESIDENT_ROLE) {
         uint256 tokenId = _tokenIdCounter;
         _tokenIdCounter += 1;
         _safeMint(to, tokenId);
