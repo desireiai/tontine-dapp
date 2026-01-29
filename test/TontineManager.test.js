4a3d51b2272a9a9d1e5aff5e4616cbb64ac6eb98
Murielle <muriellekuete@gmail.com>
2026-01-29 15:40:17 +0100

    Adaptation des tests au nouveau TontineManager qui contient le mapping

--- test/TontineManager.test.js
+++ test/TontineManager.test.js
@@ -11,27 +11,38 @@ describe("TontineManager", function () {
     [president, member1, member2] = await ethers.getSigners();
 
     const TontineManager = await ethers.getContractFactory("TontineManager");
-
     tontine = await TontineManager.deploy(
-      ethers.parseEther("1") // cotisation = 1 ETH (ethers v6)
+      ethers.parseEther("1")
     );
 
-    await tontine.waitForDeployment(); // ethers v6
+    await tontine.waitForDeployment();
   });
 
-  it("Doit définir correctement le président", async function () {
+  it("definit correctement le president", async function () {
     expect(await tontine.president()).to.equal(president.address);
   });
 
-  it("Doit permettre au président d'ajouter des membres", async function () {
+  it("permet au president d'ajouter un membre", async function () {
     await tontine.addMember(member1.address);
-    const firstMember = await tontine.members(0);
-    expect(firstMember).to.equal(member1.address);
+
+    const members = await tontine.getMembers();
+    expect(members.length).to.equal(1);
+    expect(members[0]).to.equal(member1.address);
   });
 
-  it("Un membre peut payer la cotisation", async function () {
+  it("empeche un non-membre de payer la cotisation", async function () {
     await tontine.startCycle();
+
+    await expect(
+      tontine.connect(member1).payCotisation({
+        value: ethers.parseEther("1"),
+      })
+    ).to.be.revertedWith("Not a member");
+  });
+
+  it("permet a un membre de payer la cotisation", async function () {
     await tontine.addMember(member1.address);
+    await tontine.startCycle();
 
     await expect(
       tontine.connect(member1).payCotisation({
@@ -40,16 +51,16 @@ describe("TontineManager", function () {
     ).to.not.be.reverted;
   });
 
-  it("Le président peut distribuer la tontine", async function () {
-    await tontine.startCycle();
+  it("permet au president de distribuer la tontine", async function () {
     await tontine.addMember(member1.address);
+    await tontine.startCycle();
 
     await tontine.connect(member1).payCotisation({
       value: ethers.parseEther("1"),
     });
 
     await expect(() =>
-      tontine.connect(president).distribute()
+      tontine.distribute()
     ).to.changeEtherBalance(member1, ethers.parseEther("1"));
   });
 });
